0ccad7a46377d8b21db0f0fe24156f7c704a7e40
ShangYuQiu <shangyuq@ucm.es>
2023-10-19 13:38:43 +0200

    Initial commit

---
+++ src/main/java/poker2/Poker2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package poker2;
+
+/**
+ *
+ * @author shangyu
+ */
+public class Poker2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
