4a754c9bf4e655745466c73769cdda1500fdd370
ShangYuQiu <101433056+ShangYuQiu@users.noreply.github.com>
2023-10-19 17:47:43 +0200

    Update Poker2.java

--- src/main/java/poker2/Poker2.java
+++ src/main/java/poker2/Poker2.java
@@ -1,16 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package poker2;
 
-/**
- *
- * @author shangyu
- */
+import GUI.Vista;
+
 public class Poker2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Vista v= new Vista();
+        v.setVisible(true);
     }
 }
