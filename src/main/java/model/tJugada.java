933acf73f127e0bcc6fffc566899ea0f5086d0ff
ShangYuQiu <shangyuq@ucm.es>
2023-11-01 16:30:50 +0100

    Create tJugada.java

---
+++ src/main/java/model/tJugada.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author shangyu
+ */
+//Representacion de las posibles jugadas.
+public enum tJugada {
+    CARTA_ALTA, PAREJA, DOBLE_PAREJA, TRIO, ESCALERA, COLOR, FULL_HOUSE, POKER, ESCALERA_COLOR 
+}
